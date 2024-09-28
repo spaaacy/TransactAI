a7225f189f5137c9fabcefd252fa83167a600d92
Kevin <kpluas21@gmail.com>
2024-09-28 16:18:04 -0400

    added api call

--- app/api/openai/route.js
+++ app/api/openai/route.js
@@ -1,11 +1,20 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server.js";
+import OpenAI from "openai";
+const openai =  new OpenAI(process.env.OPENAI_API_KEY);
+
 
 export async function GET(req, res) {
   try {
-
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4o",
+      messages: [
+        {"role": "user", "content": "write a haiku about ai"}
+      ]
+    });
+    console.log(completion.choices[0].message);
     // OpenAI calls goes here
     
-    return NextResponse.json({ results: ""}, { status: 200 });
+    return NextResponse.json({ results: completion.choices[0].message.content}, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error }, { status: 500 });
